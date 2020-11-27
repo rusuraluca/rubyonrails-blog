ae2bbe58ef7fecbd5bf7f14071a7e5e2aa65a6a8
Raluca Rusu <rr.ralucarusu@gmail.com>
2020-11-27 16:44:26 +0200

    updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :set_article, only: [:edit, :update, :show]
 
   def index
     @articles = Article.all
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     #@article.save
     #redirect_to articles_path(@article)
       if @article.save
-        flash[:notice] = "Article was successfully created"
+        flash[:success] = "Article was successfully created"
         redirect_to article_path(@article)
       else
         render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'update'
@@ -38,8 +38,9 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article.destroy
-      flash[:notice] = "Article was successfully deleted"
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash[:success] = "Article was successfully deleted"
       redirect_to articles_path
   end
 
