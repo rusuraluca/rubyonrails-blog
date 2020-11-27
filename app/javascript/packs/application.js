c0208d5e44ac672f1813b64bb15acee7c9bc86fb
Raluca Rusu <rr.ralucarusu@gmail.com>
2020-11-27 16:47:43 +0200

    updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,12 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-import "bootstrap"
-import "../stylesheets/application"
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
